03f9ea529fd9532da635f88d047e043ca401776c
ArsalanShaiikh <139189175+ArsalanShaiikh@users.noreply.github.com>
2026-06-25 16:58:04 +0530

    Rename Video navbar tab label to AV

--- src/components/home/HomeNavbar.jsx
+++ src/components/home/HomeNavbar.jsx
@@ -9,7 +9,7 @@ import InlineSVG from '../about/InlineSVG'
 const items = [
   { id: 'about',        label: 'About Us',     icon: '/home/svgs/about.svg',        to: '/about' },
   { id: 'location',     label: 'Location',     icon: '/home/svgs/map.svg',           to: null },
-  { id: 'video',        label: 'Video',        icon: '/home/svgs/video.svg',         to: '/video' },
+  { id: 'video',        label: 'AV',           icon: '/home/svgs/video.svg',         to: '/video' },
   { id: 'amenities',    label: 'Amenities',    icon: '/home/svgs/aminities.svg',     to: '/gallery' },
   { id: 'towers',       label: 'Towers',       icon: '/home/svgs/towers.svg',        to: '/towers' },
   { id: 'construction', label: 'Construction', icon: '/home/svgs/construction.svg',  to: '/construction' },
